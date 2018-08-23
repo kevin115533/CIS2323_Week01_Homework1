067550ea071d7ddfbe4e4e6666bedf2c53302e44
kevin115533 <35547586+kevin115533@users.noreply.github.com>
2018-08-23 14:29:23 -0500

    Terminal business card
    added student information

--- CIS2323_Week01_Homework1.java
+++ CIS2323_Week01_Homework1.java
@@ -1,5 +1,12 @@
 public class CIS2323_Week01_Homework1 {
 	public static void main(String[] args){
-		System.out.println("Test");
+		System.out.println("--------------------------------------");
+		System.out.println("              Kevin Tran");
+		System.out.println("        kevin115533@yahoo.com");
+		System.out.println("        github.com/kevin115533");
+		System.out.println("            405-926-0131");
+		System.out.println("");
+		System.out.println("  Student,OSU-OKC    Server/Bartender ");
+		System.out.println("--------------------------------------");
 	}
 }
